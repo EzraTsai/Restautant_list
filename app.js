cb4cfac7e8bddab5008a33062bd86b7352f85252
Ezra Tsai <ishow8166@icloud.com>
2020-07-23 14:08:11 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 //require 
 const restaurantList = require('./restaurant.json')
+
+// 取得資料庫連線狀態
+const db = mongoose.connection 
+// 連線異常
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
+
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
