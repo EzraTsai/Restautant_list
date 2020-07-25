f5d5445a21c2489fdecdb02e54d8c7584e286564
Ezra Tsai <ishow8166@icloud.com>
2020-07-25 14:25:00 +0800

    feat: fix空欄位未填就送出 and 刪除資料前的提醒

--- app.js
+++ app.js
@@ -13,10 +13,9 @@ mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true,
 const exphbs = require('express-handlebars')
 //require 
 const restaurantList = require('./restaurant.json')
-const restaurant = require('./models/restaurant')
 
 // 取得資料庫連線狀態
-const db = mongoose.connection 
+const db = mongoose.connection
 // 連線異常
 db.on('error', () => {
     console.log('mongodb error!')
@@ -34,14 +33,14 @@ app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
 
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //index page route setting
 app.get('/', (req, res) => {
     Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index',{ restaurants }))
-    .catch(error => console.log)
+        .lean()
+        .then(restaurants => res.render('index', { restaurants }))
+        .catch(error => console.log)
 })
 
 //search-bar route setting
@@ -49,78 +48,53 @@ app.get('/search', (req, res) => {
     const keyword = req.query.keyword
     const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
     res.render('index', { restaurants: restaurants, keyword: keyword })
-  })
+})
 
 app.get('/restaurants/new', (req, res) => {
     return res.render('new')
-})  
+})
 
 app.post('/restaurants', (req, res) => {
-    const name = req.body.name
-    const name_en = req.body.name_en
-    const category = req.body.category
-    const image = req.body.image
-    const location = req.body.location
-    const phone = req.body.phone
-    const google_map = req.body.google_map
-    const rating = req.body.rating
-    const description = req.body.description
-    return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    const restaurant = req.body
+    return Restaurant.create(restaurant)
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
 })
 
 app.get('/restaurants/:id/edit', (req, res) => {
     const id = req.params.id
     return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
+        .lean()
+        .then((restaurant) => res.render('edit', { restaurant }))
+        .catch(error => console.log(error))
 })
 
 app.post('/restaurants/:id/edit', (req, res) => {
     const id = req.params.id
-    const name = req.body.name
-    const name_en = req.body.name_en
-    const category = req.body.category
-    const image = req.body.image
-    const location = req.body.location
-    const phone = req.body.phone
-    const google_map = req.body.google_map
-    const rating = req.body.rating
-    const description = req.body.description
     return Restaurant.findById(id)
-    .then(restaurant => {
-        restaurant.name = name
-        restaurant.name_en = name_en
-        restaurant.category = category
-        restaurant.image = image
-        restaurant.location = location
-        restaurant.phone = phone
-        restaurant.google_map = google_map
-        restaurant.rating = rating
-        restaurant.description = description
-        return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
+        .then(restaurant => {
+            restaurant = Object.assign(restaurant, req.body)
+            return restaurant.save()
+        })
+        .then(() => res.redirect(`/restaurants/${id}`))
+        .catch(error => console.log(error))
 })
 
 app.post('/restaurants/:id/delete', (req, res) => {
     const id = req.params.id
     return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+        .then(restaurant => restaurant.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
 })
 
 //showpage route setting
 app.get('/restaurants/:id', (req, res) => {
     const id = req.params.id
     return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
+        .lean()
+        .then((restaurant) => res.render('show', { restaurant }))
+        .catch(error => console.log(error))
 })
 
 // start and listen on the Express server
