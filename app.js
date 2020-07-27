8c6628e5ad2548e5a248f28c61ad3a707c587e6f
Ezra Tsai <ishow8166@icloud.com>
2020-07-27 20:54:16 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-const port = 3000
+require('./config/mongoose')
 
 const app = express()
-
-const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-// require express-handlebars here
-const exphbs = require('express-handlebars')
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
-
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// setting static files
 app.use(express.static('public'))
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
-
 app.use(routes)
 
-// start and listen on the Express server
-app.listen(port, () => {
-    console.log(`Express is listening on local host: ${port} `)
+app.listen(3000, () => {
+    console.log('Express is listening on local host: 3000')
 })
