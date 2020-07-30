a957c395f4e0c03a0a8e4df1deca2475317e1f41
Ezra Tsai <ishow8166@icloud.com>
2020-07-30 13:03:58 +0800

    fix: sort function

--- routes/modules/sort.js
+++ routes/modules/sort.js
@@ -33,36 +33,4 @@ router.get('/rating', (req, res) => {
         .then(restaurants => res.render('index', { restaurants }))
         .catch(error => console.log(error))
 })
-
-router.get('/restaurants/:id', (req, res) => {
-    const id = req.params.id
-    return Restaurant.findById(id)
-        .lean()
-        .then((restaurant) => res.render('show', { restaurant }))
-        .catch(error => console.log(error))
-})
-
-router.get('/restaurants/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Restaurant.findById(id)
-        .lean()
-        .then((restaurant) => res.render('edit', { restaurant }))
-        .catch(error => console.log(error))
-})
-
-router.delete('/delete/:id', (req, res) => {
-    const id = req.params.id
-    return Restaurant.findById(id)
-        .then(restaurant => restaurant.remove())
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-})
-
-router.post('/', (req, res) => {
-    const restaurant = req.body
-    return Restaurant.create(restaurant)
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-})
-
 module.exports = router
